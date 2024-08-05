6cbca5c0a18cc2a67613b4f6fd630c6ec16c6b92
JosephThien2000 <n.t.m.thien20.04@gmail.com>
2024-08-05 22:57:19 +0700

    Update Home, Contact, PrivacyPolicy, add ApplicationUser, Page, Post, ApplicationDbContext, WebsiteRole, IDbInitializer, DbInitializer, PageController, About, Contact,  Edit Program, Index, _Layout and some dependencies.

---
+++ KataBlogger/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace KataBlogger.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        //relation
+        public List<Post>? Posts { get; set; }
+    }
+}
