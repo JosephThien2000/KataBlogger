b97ec506122695baba7f8df198686ab61713fd12
JosephThien2000 <n.t.m.thien20.04@gmail.com>
2024-08-07 23:20:53 +0700

    update login and logout features

--- KataBlogger/Areas/Admin/Controllers/UserController.cs
+++ KataBlogger/Areas/Admin/Controllers/UserController.cs
@@ -1,11 +1,25 @@
+using AspNetCoreHero.ToastNotification.Abstractions;
+using KataBlogger.Models;
 using KataBlogger.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KataBlogger.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly INotyfService _notification;
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+                                INotyfService notyfService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _notification = notyfService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,7 +28,43 @@ namespace KataBlogger.Areas.Admin.Controllers
         [HttpGet("Login")]
         public IActionResult Login()
         {
-            return View(new LoginVM());
+            if(!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return View(new LoginVM());
+            }
+            return RedirectToAction("Index", "User", new {area="Admin"});
+        }
+
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginVM vm)
+        {
+            if(!ModelState.IsValid) { return View(vm);}
+            var existingUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == vm.Username);
+            if(existingUser == null)
+            {
+                _notification.Error("Username does not exist");
+                return View(vm);
+            }
+            
+            var verifyPassword = await _userManager.CheckPasswordAsync(existingUser, vm.Password);
+            if(!verifyPassword)
+            {
+                _notification.Error("Incorrect password");
+                return View(vm);
+            }
+
+            await _signInManager.PasswordSignInAsync(vm.Username, vm.Password, vm.RememberMe, true);
+            _notification.Success("Login Successful");
+            return RedirectToAction("Index", "User", new {area="Admin"});
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            _notification.Success("Logout Successful");
+            return RedirectToAction("Index", "Home", new {area=""});
         }
     }
 }
--- KataBlogger/obj/Debug/net8.0/KataBlogger.AssemblyInfo.cs
+++ KataBlogger/obj/Debug/net8.0/KataBlogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KataBlogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b65b74b74534a6ed95a15579008ab5a2291dc5da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c1f1f25163cb540de587434f39660d5e3c746d")]
 [assembly: System.Reflection.AssemblyProductAttribute("KataBlogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KataBlogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
