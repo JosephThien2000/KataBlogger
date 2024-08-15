a4d12bea358cce71dc267c3ee1b43750af0d209c
TuanAnh <doantuananh16122002@gmail.com>
2024-08-15 13:31:13 +0700

    setting controller, setting model, and settingVM

--- KataBlogger/obj/Debug/net8.0/KataBlogger.AssemblyInfo.cs
+++ KataBlogger/obj/Debug/net8.0/KataBlogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KataBlogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+274d228029c55f635a8d4649f0bef982cd808aa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5bc0a1850387048395d56c38db145229dbdaab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("KataBlogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KataBlogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
